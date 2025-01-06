2f14075415ce14bf0fda2c6c76c10273c1d39ebe
PrathamChhabra04 <prathamchhabraa@gmail.com>
2025-01-06 20:10:22 +0530

    footer yr change

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <Toaster richColors />
           <footer className="bg-blue-50 py-8">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>© 2024 Pratham Chhabra. All rights reserved.</p>
+              <p>© 2025 Pratham Chhabra. All rights reserved.</p>
             </div>
           </footer>
         </body>
